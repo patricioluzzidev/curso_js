172b5152ba0ceaedebcaff312c4e8fa3c8d240d1
luzzirommina <luzziromina@gmail.com>
2023-10-21 12:14:38 -0300

    Sacar Math.round

--- js/app.js
+++ js/app.js
@@ -79,7 +79,7 @@ function drawContainerWeather(data){
   <div class='row main__content__today'>
     <div class='col-12 text-center'>
       <i class="wi ${weatherIcon(weatherDesctiption)}"></i>
-      <h3>${weatherMain ? weatherMain :  "No Data"} | ${ temp_max ? Math.round(temp_max) : "0" }° | ${ temp_min ? Math.round(temp_min) : "0" }°</h3>
+      <h3>${weatherMain ? weatherMain :  "No Data"} | ${ temp_max ? temp_max : "0" }° | ${ temp_min ? temp_min : "0" }°</h3>
       ${ weather && `<button class='btn btn-primary' id='more-details-btn'>More details</button>`}
       </div>
   </div>
@@ -138,7 +138,7 @@ function drawContainerForescast(data){
         <div class='col-12 col-md-1 text-center'>
           <h2>${ date }hs</h2>
           <i class="wi ${weatherIcon( weatherDesctiption || "")}"></i>
-          <h3>${ temp_max ? Math.round(temp_max) : "0" }° | ${ temp_min ? Math.round(temp_min) : "0" }°</h3>
+          <h3>${ temp_max ? temp_max : "0" }° | ${ temp_min ? temp_min : "0" }°</h3>
         </div>`;
       })
     }
